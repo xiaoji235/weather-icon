7aac0b3eed425fae4c51af7482815abe90e51af7
小基同學 <73387366+xiaoji235@users.noreply.github.com>
2025-09-24 14:00:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -58,5 +58,6 @@ async def get_weather_svg():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+    uvicorn.run(app, host="0.0.0.0", port=443)
     
+
